f900a27e3fe0a2216167c986381b5b6963c06732
RuanRodrigues1 <142950355+RuanRodrigues1@users.noreply.github.com>
2025-02-15 20:20:17 -0300

    Update user.py
    corrigindo o acesso do fake_db para o MySqlDataBase

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -1,16 +1,19 @@
 from typing import List, Optional
 from schemas.user import UserInDB
-from db.fake_db import FakeDataBase
+from db.mySQL_db import MySQLDatabase
 from security import verify_password
-
 
 
 class UserRepository:
     def __init__(self):
-        self.user_db = FakeDataBase(UserInDB)
-        user = UserInDB(email="user@example.com", full_name="string",
-                        hashed_password="$2b$12$76sdl6It7zWmRSVIUofC0eDE0ujtr6G/VC4U6uCkp1V8ldzEbJIL2")
-        self.user_db.add_item(user)
+        self.db = MySQLDatabase()
+
+    def get_user(self, user_email: str) -> Optional[UserInDB]:
+        query = "SELECT * FROM users WHERE email = %s"
+        result = self.db.fetch_one(query, (user_email,))
+        if result:
+            return UserInDB(email=result[1], full_name=result[2], type=result[3], hashed_password=result[4])
+        return None
 
     def get_all_users(self) -> List[UserInDB]:
         return self.user_db.get_all_items()
