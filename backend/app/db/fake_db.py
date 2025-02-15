e6875bb13a24203d9b1eb9ba0d06f0620744cbb9
RuanRodrigues1 <142950355+RuanRodrigues1@users.noreply.github.com>
2025-02-15 20:17:57 -0300

    Update fake_db.py
    criada a classe mySQL_db para criar conectores de acesso ao banco de dados mysql.

--- backend/app/db/fake_db.py
+++ backend/app/db/fake_db.py
@@ -1,32 +1,27 @@
-from typing import Type, TypeVar, Generic, List, Any, Optional
+import mysql.connector
 
-T = TypeVar("T")
+class MySQLDatabase:
+    def __init__(self):
+        self.connection = mysql.connector.connect(
+            host="localhost",
+            user="seu_usuario",
+            password="sua_senha",
+            database="sustainable_life"
+        )
+        self.cursor = self.connection.cursor()
 
+    def execute_query(self, query, params=None):
+        self.cursor.execute(query, params or ())
+        self.connection.commit()
 
-class FakeDataBase(Generic[T]):
-    def __init__(self, model: Type[T]):
-        self.model = model
-        self.data: List[T] = []
+    def fetch_all(self, query, params=None):
+        self.cursor.execute(query, params or ())
+        return self.cursor.fetchall()
 
-    def get_all_items(self) -> List[T]:
-        return self.data
+    def fetch_one(self, query, params=None):
+        self.cursor.execute(query, params or ())
+        return self.cursor.fetchone()
 
-    def add_item(self, item: T):
-        self.data.append(item)
-
-    def delete_item(self, item_id: Any, id_field: str = "id"):
-        self.data = [item for item in self.data if getattr(
-            item, id_field, None) != item_id]
-
-    def item_exists(self, value: Any, field: str = "id") -> bool:
-        return any(getattr(item, field, None) == value for item in self.data)
-
-    def get_item(self, value: Any, field: str = "id") -> Optional[T]:
-        for item in self.data:
-            if getattr(item, field, None) == value:
-                return item
-        return None
-
-    def update_item(self, item_id: Any, id_field: str, item: T):
-        self.delete_item(item_id, id_field)
-        self.add_item(item)
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
