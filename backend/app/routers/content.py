631295e5b93cd0b25afc41a36576081ec3d9086a
Ricleyson <ricleyson3.0@hotmail.com>
2025-02-17 11:21:46 -0300

    merge

--- backend/app/routers/content.py
+++ backend/app/routers/content.py
@@ -11,18 +11,18 @@ async def get_contents() -> Contents:
 
 
 @router.get("/{content_id}", response_class=HTMLResponse)
-async def get_content_html(content_id: int) -> str:
+async def get_content_html(content_id: int):
     content = content_crud.get_content(content_id)
     if not content:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Content with {content_id} not found"
         )
-    return content.html
+    return HTMLResponse(content=content.html)
 
 
-@router.post("/", response_model= Content ,status_code=status.HTTP_201_CREATED)
-async def create_content(content_create: Content) -> Content:
+@router.post("/", status_code=status.HTTP_204_NO_CONTENT)
+async def create_content(content_create: Content):
     content = content_crud.get_content(content_create.id)
     if content is not None:
         raise HTTPException(
@@ -30,4 +30,3 @@ async def create_content(content_create: Content) -> Content:
             detail=f"The content with this {content_create.id} already exists in the system",
         )
     content_crud.create_content(content_create)
-    return content_create
